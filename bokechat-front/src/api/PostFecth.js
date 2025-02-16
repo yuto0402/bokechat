08de290f4a205c0c41157722aaa1e76e16e68fed
yuto0402 <yamada.yuto.54x@st.kyoto-u.ac.jp>
2025-02-16 16:45:33 +0900

    PostFecth.js を更新

--- bokechat-front/src/api/PostFecth.js
+++ bokechat-front/src/api/PostFecth.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export const postFecth = async ({ pageParam = 1 }) => {
   try {
     const res = await axios.get(`http://localhost:8000/api/posts/?page=${pageParam}`);
-    return res;
+    return res.data;
   } catch (error) {
     console.error("Error fetching posts:", error);
     throw new Error("Failed to fetch posts"); // エラーメッセージを投げる
